91329737b30569c3127d53c8aff19a10b2e0dc0b
NickNish09 <nicholasmarques@cjr.org.br>
2017-07-19 12:40:59 -0300

    criado AlbumList com requisicao ajax

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make Component
-const Header = (props) =>{
-	const {textStyle, viewStyle} = styles;
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
 	return (
 		<View style = {viewStyle}>
 			<Text style = {textStyle}>{props.headerText}</Text>
